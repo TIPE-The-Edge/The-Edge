41b3bf2de7f4f3f864fb180068b926f36bb6fdec
Maxence Blanc <maxence.blanc@eisti.eu>
2017-11-19 21:10:01 +0100

    création de la class Individu

--- generators.py
+++ generators.py
@@ -9,7 +9,7 @@
 ########################
 
 # IMPORTS
-from random import *
+import random
 import os
 
 # IMPORTS DE FICHIERS
@@ -45,37 +45,88 @@ Vérifié par :
 
 
 ####################################################
-##################| FONCTIONS |#####################
+###################| CLASSES |######################
 ####################################################
 
-def genName(fichier_prenom, fichier_nom):
-    """ Retourne aléatoirement un prénom avec son nom de famille
-    a partir d'un fichier de noms.
-    Entrée :
-    Variables :
-    Sortie :
-    Vérifié par :
+class Individu(object):
+    """ Class contenant toutes les informations concernant un individu.
+
+    Nom, Prénom
+    Salaire
+    Bonheur
+    Heure de travail ? (rentre dans le bonheur ?)
+    Statut (stagiaire?, cdd, cdi)
+    Rôle (Inactif, Marketing, production, chercheur, etc.)
+    Age (retraite)
+    Nombre de jours de congés (maladie, vacances, etc.)*
+    Projet en cours
+
+    Expérience (influence sur les compétences):
+        Expérience dans la start up elle même.
+        Expérience sur un certain produit (bonus s’il travail sur son amélioration etc.)
+        Expérience pour une certaine fonction de l’entreprise.
+
+    Compétences :
+        Travail de groupe
+        Recherche
+        Gestion, Management
+        Logistique
+        Marketing
     """
 
-    # Names
-    entree = open(fichier_prenom,"r") # Fichier de score voulu
-    contenu_entree = entree.readlines()
-    entree.close()
+    def __init__(self):
 
-    NAMES = [ligne.strip('\n') for ligne in contenu_entree]
+        self.genre   = random.choice(["homme", "femme"])
 
-    # Family names
-    entree = open(fichier_nom,"r") # Fichier de score voulu
-    contenu_entree = entree.readlines()
-    entree.close()
+        # Nom, Prénom
+        if self.genre == "homme" :
+            self.prenom  = self.genName("./Name_Files/boy_names.txt")
+        else : self.prenom  = self.genName("./Name_Files/girl_names.txt")
+        self.nom     = self.genName("./Name_Files/family_names.txt")
 
-    FAMILY_NAMES = [ligne.strip('\n') for ligne in contenu_entree]
+        self.age     = 0
 
-    # Corps de la fonction
+        self.salaire = 0
+        self.bonheur = 0
 
-    ran1 = randint(0, len(NAMES)-1)
-    ran2 = randint(0, len(FAMILY_NAMES)-1)
-    return(NAMES[ran1]+" "+FAMILY_NAMES[ran2])
+        self.statut  = None
+        self.role    = None
+        self.conges  = 0
+        self.horaire = 0 # temps de travail
+
+        self.projet  = None
+
+        # Experiences
+        self.exp_startup = 0
+        self.exp_produit = []
+        self.exp_role    = []
+
+        # Compétences
+        self.competence_groupe      = 0
+        self.competence_recherche   = 0
+        self.competence_gestion     = 0
+        self.competence_logistique  = 0
+        self.competence_marketing   = 0
+
+        individus.append(self)
+
+    def genName(self, fichier):
+        """ Retourne aléatoirement une ligne d'un fichier.
+        ENTREE      : un fichier.txt
+        VARIABLES   : lignes : la liste des lignes du fichier
+        SORTIE      : une string correspondant à une ligne du fichier
+        Vérifié par :
+        """
+
+        # Lecture du fichier
+        entree = open(fichier,"r") # Fichier voulu
+        contenu_entree = entree.readlines()
+        entree.close()
+
+        # On créé une liste qui contient toutes les lignes du fichier.
+        lignes = [ligne.strip('\n') for ligne in contenu_entree]
+
+        return(random.choice(lignes))
 
 
 
@@ -85,6 +136,11 @@ def genName(fichier_prenom, fichier_nom):
 
 if __name__ == "__main__" :
 
+    individus = [] # liste des individus
+
     os.system('clear') # works on Linux/Mac
-    print(genName("./Name_Files/boy_names.txt", "./Name_Files/family_names.txt"))
-    print(genName("./Name_Files/girl_names.txt", "./Name_Files/family_names.txt"))
+    Bob = Individu()
+    print(Bob.prenom, Bob.nom, Bob.age)
+    print(Bob.genre, Bob.role)
+
+    print(individus)
